6eb1ecd2d08c1efa566cb8e89ba80b0cdb3962f9
Percy Leon Lucas <Percy_Lucas@satrack.com>
2022-03-29 23:57:41 -0500

    formularios Percy Leon

---
+++ app/src/main/java/com/example/fast_service/InfoProductActivity.java
@@ -0,0 +1,14 @@
+package com.example.fast_service;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InfoProductActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_product);
+    }
+}
--- app/src/main/java/com/example/fast_service/LoginActivity.java
+++ app/src/main/java/com/example/fast_service/LoginActivity.java
@@ -2,7 +2,11 @@ package com.example.fast_service;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void iniciarSesion(View v){
+        EditText user = findViewById(R.id.editTextTextUser);
+        EditText password = findViewById(R.id.editTextTextPassword);
+
+        String x = user.getText().toString();
+        String y = password.getText().toString();
+
+        Log.i("===>", x);
+
+        startActivity(new Intent(this, MainActivity.class));
     }
 }
